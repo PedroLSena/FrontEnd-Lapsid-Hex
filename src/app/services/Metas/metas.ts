937595abc7b2e190d46f07a5a264f8828fedc93a
Pedro Lucas <pes.ex20000@gmail.com>
2025-06-06 20:59:02 -0300

    All meta crud api Add

--- src/app/services/Metas/metas.ts
+++ src/app/services/Metas/metas.ts
@@ -11,5 +11,40 @@ export const metasServise ={
         }catch(error:any){
             throw new Error("Erro ao consumir api", error);
         }
+    },
+
+    async getMeta(id: number):Promise<GerenciamentoMeta[]>{
+        try {
+            const response  = await api.get(`GerenciamentoMeta/${id}`);
+            return response.data;
+        }catch(error:any){
+            throw new Error("Erro ao consumir api", error);
+        }
+    },
+    async deleteMeta(id:number):Promise<GerenciamentoMeta[]>{
+        try{
+            const response = await api.delete(`GerenciamentoMeta/${id}`);
+            return response.data;
+        }catch(error:any){
+            throw new Error("Erro ao consumir api", error);
+        }
+    },
+    async postMeta(userData:Omit<GerenciamentoMeta, 'id'>):Promise<GerenciamentoMeta[]>{
+        try{
+            const response = await api.post(`GerenciamentoMeta/${userData}`);
+            return response.data;
+        }catch(error: any){
+            throw new Error("Erro ao consumir api", error);
+        }
+    },
+
+    async putMeta(id: number, userData:Partial<GerenciamentoMeta>):Promise<GerenciamentoMeta[]>{
+        try{
+            const response = await api.put(`GerenciamentoMeta/${userData}`);
+            return response.data;
+        }catch(error: any){
+            throw new Error("Erro ao consumir api", error);
+        }
+
     }
 }
